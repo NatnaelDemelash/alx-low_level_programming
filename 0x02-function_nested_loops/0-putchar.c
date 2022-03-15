c3ba0f021307db5f145641f3702aac0d059bf7c6
NatnaelDemelash <natidemelash074@gmail.com>
2022-03-15 11:51:06 -0700

    remove unnecessary readme

---
+++ 0x02-function_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print _putchar.
+ * Return: 0
+ */
+int main(void)
+{
+	char c[] = "_putchr";
+
+	putchar(c);
+	putchar('\n');
+
+	return (0);
+}
